7ca524b140d9f0f12a16039e80611973b2878a03
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-11-08 12:17:04 -0800

    added code

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-$(document).ready(function() {
-    $("#square").click(function() {
-      $("#square").fadeOut(2000, alert2);
-    });
-    $(this).keypress(function(e) {
-      switch(e.keyCode) {
-        case 32:
-          animateRec();
-          break;
-      }
-    });
-    function alert2(){
-      animation();
-      alert("Im done!");
-      $("#square").css({"background-color":"green"});
-      $("#square").fadeIn(2000);
-    }
-  
-    function animation() {
-      $("#object").animate({"border-radius":"0px"}, 10000);
-    }
-  
-    function animateRec() {
-      $("#keyPress").animate({"border-radius":"80px"}, 10000);
-    }
-  
-  });
+$(document).ready(function() {exec();})
+function exec() {
+    $("#cubeo").click(function() {$("#cubeio").animate({"border-radius":"100px"}, 800);});
+}
