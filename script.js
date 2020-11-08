a63ac511a1ffcbe569d911e76cdef1b8d1d446ff
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-11-08 11:55:12 -0800

    Added Template

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-    $("#obj1").click(function() {
-        $("#obj1").animate({"border-radius":"80px"}, 1000)
-    })
-}
+    $("#square").click(function() {
+      $("#square").fadeOut(2000, alert2);
+    });
+    $(this).keypress(function(e) {
+      switch(e.keyCode) {
+        case 32:
+          animateRec();
+          break;
+      }
+    });
+    function alert2(){
+      animation();
+      alert("Im done!");
+      $("#square").css({"background-color":"green"});
+      $("#square").fadeIn(2000);
+    }
+  
+    function animation() {
+      $("#object").animate({"border-radius":"0px"}, 10000);
+    }
+  
+    function animateRec() {
+      $("#keyPress").animate({"border-radius":"80px"}, 10000);
+    }
+  
+  });
