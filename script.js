3949050e4a677712beca2ebdacf35c85dd46b6d7
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-11-08 16:24:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {exec();})
-function exec() {
-    $("#cubeo").click(function() {$("#cubeio").animate({"border-radius":"100px"}, 800);});
-}
+$(document).ready(function() {
+    var pxVar;
+    function mvRight() {
+        alert(pxVar);
+        $("#cubeo").css("margin-left", pxVar);
+    }
+    function keyDownHandler(e) {
+        if (e.key == "ArrowRight") {
+            pxVar = $(this).css("margin-left");
+            mvRight();
+        }
+    }
+    $("#cubeo").click(function() {
+        $("#cubeo").animate({"border-radius":"20px"}, 1000);
+    })
+    document.addEventListener("keydown", keyDownHandler, false);
+})
